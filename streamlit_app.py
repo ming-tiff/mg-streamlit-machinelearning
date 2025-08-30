34087e71de4a921654485dd24bce3ec9a332f8f3
ming-tiff <min.gis.infinity@gmail.com>
2025-08-30 21:51:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,8 @@ data = {'island': island,
         'flipper_length_mm': flipper_length_mm,
         'body_mass_g': body_mass_g,
          'gender': gender}
-  input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X], axis=0)
+input_df = pd.DataFrame(data, index=[0])
+input_penguins = pd.concat([input_df, X], axis=0)
 
 with st.expander('Input features'):
   st.write('**Input penguin**')       
