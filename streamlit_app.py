e7111df7d47f2237bf85de1be8ee8bd4e31ee453
ming-tiff <min.gis.infinity@gmail.com>
2025-08-30 21:12:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,6 @@ data = {'island': island,
         'body_mass_g': body_mass_g,
          'gender': gender}
 input_df = pd.DataFrame(data, index=[0])
-input_peguins = pd.concat([input.df, X], axis=0)
-input_df
-  
+input_penguins = pd.concat([input.df, X], axis=0)
+input_penguins
+
