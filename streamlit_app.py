a1128e34f364998bcd05903a85f5cf34b377ec05
ming-tiff <min.gis.infinity@gmail.com>
2025-08-30 11:31:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd 
 
 st.title('🤖 Machine Leaning App')
 
 st.info('This is a machine Learning App!')
+
+df = pd.reascsv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df
