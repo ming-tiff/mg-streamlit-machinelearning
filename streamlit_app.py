8fb72d90c9b6bfe99ba0d5e14a878e14387da52e
ming-tiff <min.gis.infinity@gmail.com>
2025-08-30 21:38:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,9 @@ input_df = pd.DataFrame(data, index=[0])
 input_penguins = pd.concat([input_df, X], axis=0)
 input_penguins
 
-input_df
+with st.expander('Input features')
+  st.write('**Input penguin**')       
+  input_df
+  st.write('**Combined penguins data**')
+  input_penguins
 # input_peguins
