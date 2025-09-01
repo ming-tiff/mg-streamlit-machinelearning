dc692f97fd1418a6eb26554813b7a21063e76aa5
ming-tiff <min.gis.infinity@gmail.com>
2025-09-01 16:28:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ data = {'island': island,
         'bill_depth_mm': bill_depth_mm,
         'flipper_length_mm': flipper_length_mm,
         'body_mass_g': body_mass_g,
-         'gender': gender}
+         'sex': gender}
 input_df = pd.DataFrame(data, index=[0])
 input_penguins = pd.concat([input_df, X], axis=0)
 
@@ -53,6 +53,6 @@ with st.expander('Input features'):
   input_penguins
 
 #Encode
-encode = ['island', 'gender']
+encode = ['island', 'sex']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
 df_penguins[:1]
