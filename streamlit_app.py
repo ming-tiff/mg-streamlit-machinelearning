029ea872660532b3b5857004ea6cd92015820236
ming-tiff <min.gis.infinity@gmail.com>
2025-09-01 11:41:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,3 +52,7 @@ with st.expander('Input features'):
   st.write('**Combined penguins data**')
   input_penguins
 
+#Encode
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+df_penguins[:1]
